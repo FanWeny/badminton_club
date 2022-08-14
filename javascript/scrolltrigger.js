5064a61f60581fdcff3e36ef084e345e9e4d1a88
FanWeny <z04wj06y7@gmail.com>
2022-08-14 17:34:32 +0800

    gsap update

---
+++ javascript/scrolltrigger.js
@@ -0,0 +1,40 @@
+let minWidth = window.innerWidth
+
+
+
+if(minWidth > 768){
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".pic_court", {
+        scrollTrigger: ".pic_court",
+        x: -500,
+        opacity: 1,
+        duration: 2,
+    });
+    gsap.to(".court_first_content", {
+        scrollTrigger: ".court_first_content",
+        x: 500,
+        opacity: 1,
+        duration: 2,
+    });
+
+
+    gsap.to(".rese_left", {
+        scrollTrigger: ".rese_left",
+        x: 300,
+        opacity: 1,
+        duration: 2,
+    });
+    gsap.to(".rese_center", {
+        scrollTrigger: ".rese_center",
+        x: 600,
+        opacity: 1,
+        duration: 2,
+    });
+    gsap.to(".rese_right", {
+        scrollTrigger: ".rese_right",
+        x: 900,
+        opacity: 1,
+        duration: 2,
+    });
+}
